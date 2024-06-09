f4a5834e580d63295865b10d13e70e4930cab04f
Himanshu Chuchra <chuchrahimanshu@gmail.com>
2024-06-09 12:43:49 +0530

    Setup cloudinary for handling media files

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import "dotenv";
 import "./config/database.config.js";
 import "./config/nodemailer.config.js";
+import "./config/cloudinary.config.js";
 
 // Configuration Section
 const app = express();
